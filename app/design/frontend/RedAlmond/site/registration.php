d0cc0a258cede4f9092f5a5e1a69c89ba6b49dc4
Ezequiel Alba <ezequielalba@gmail.com>
2019-11-17 22:20:40 -0800

    Theme Declaration

---
+++ app/design/frontend/RedAlmond/site/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Blue Acorn iCi && Ezequiel Alba github@ezequielalba
+ * Use of this material is prohibited without written approval of the author.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/RedAlmond/site', __DIR__);
