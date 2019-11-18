90cdfad07b8406a2bd09a507b40c6590c3af439c
Ezequiel Alba <ezequielalba@gmail.com>
2019-11-18 13:45:16 -0800

    Create CMS page module

---
+++ app/code/RedAlmond/Cms/Setup/Patch/Data/NewPage.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright © Blue Acorn iCi && Ezequiel Alba github@ezequielalba
+ * Use of this material is prohibited without written approval of the author.
+ */
+
+namespace RedAlmond\Cms\Setup\Patch\Data;
+
+use Magento\Cms\Model\PageFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+/**
+ * Class NewPage
+ * @package RedAlmond\Cms\Setup\Patch\Data
+ */
+class NewPage implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+ 
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+ 
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        PageFactory $pageFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function apply()
+    {
+        $cmsContent = '
+            <style>.grid {  display: grid; grid-template-columns: 1fr 1fr 1fr; grid-gap: 40px; }</style>
+            <h1>BUTTONS</h1>
+            <div class="grid">
+                <div class="col">
+                    <button class="button primary tocart" type="button">
+                        <span><span>Load More</span></span>
+                    </button>
+                </div>
+                <div class="col">
+                    <button class="button btn-continue" title="Continue Shopping" type="button">
+                        <span><span>Continue Shopping</span></span>
+                    </button>
+                </div>
+                <div class="col">
+                    <button class="action primary checkout" type="button">
+                        Checkout
+                    </button>
+                </div>
+            </div>
+        ';
+
+        $cmsPageData = [
+            'title' => 'Red Almond Custom Buttons',
+            'page_layout' => '1column',
+            'meta_keywords' => 'Blue Acorn iCi Dev Test',
+            'meta_description' => 'Red Almond Custom Buttons',
+            'identifier' => 'buttons-custom-page',
+            'content_heading' => '',
+            'content' => $cmsContent,
+            'layout_update_xml' => '',
+            'url_key' => 'red-almond-custom-buttons',
+            'is_active' => 1,
+            'stores' => [0],
+            'sort_order' => 0
+        ];
+ 
+        $this->moduleDataSetup->startSetup();
+        $this->pageFactory->create()->setData($cmsPageData)->save();
+        $this->moduleDataSetup->endSetup();
+    }
+ 
+    public static function getDependencies()
+    {
+        return [];
+    }
+ 
+    public static function getVersion()
+    {
+        return '1.0.0';
+    }
+ 
+    public function getAliases()
+    {
+        return [];
+    }
+}
---
+++ app/code/RedAlmond/Cms/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Blue Acorn iCi && Ezequiel Alba github@ezequielalba
+ * Use of this material is prohibited without written approval of the author.
+ */
+
+ use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'RedAlmond_Cms', __DIR__);
